6cd6b39f3d8446cf9d4fd204140fe3bad18c890d
student <jessewang@billericak12.com>
2020-03-11 11:32:09 -0400

    Commit

---
+++ src/com/company/DocumentGlossary.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import javax.swing.text.Document;
+import java.util.ArrayList;
+
+public class DocumentGlossary extends ArrayList<GlossaryEntry> {
+    public DocumentGlossary(){
+        super();
+    }
+
+    public DocumentGlossary(int size){
+        super(size);
+    }
+
+    //public void addWord(String word, int num){ }
+
+    private int foundOrInserted(String word){
+
+        
+    }
+}
